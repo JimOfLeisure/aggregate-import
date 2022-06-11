58504f653485f46ff48ed100c51551137922f932
Jim Nelson <jim@jimnelson.us>
2022-06-10 18:30:54 -0700

    cleanup

--- Dashboard.mjs
+++ Dashboard.mjs
@@ -1,13 +1,2 @@
 export { default as DashboardBlbost } from "./DashboardBlbost.mjs";
 export { default as DashboardIndex } from "./DashboardIndex.mjs";
-
-// export DashboardIndex from "./DashboardIndex";
-// export DashboardBlbost from "./DashboardBlbost";
-
-// export DashboardIndex;
-// export DashboardBlbost;
-
-// console.log(DashboardIndex());
-// console.log(DashboardBlbost());
-
-// export { DashboardIndex, DashboardBlbost };
